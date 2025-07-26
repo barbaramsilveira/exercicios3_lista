df7dc3f0d9d343e64174dfcee566158bd9c3e2b2
Barbara Martins <barbarasilveirax@gmail.com>
2025-07-25 22:57:54 -0300

    ajustes no CSS e JS

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 /*lógica menu hamburguer*/
+const menuButton = document.getElementById('menu-button');
+    const menu = document.getElementById('menu');
+
+    menuButton.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
